167d45265674c9456a637074369cef49aee1b678
PavanMeka09 <Mekapavan99@gmail.com>
2026-01-28 11:54:50 +0530

    add prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
+globalThis.prisma = prisma;
