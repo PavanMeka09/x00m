1763be885af0f86bfa3360115fd1daa1aad9c7d4
PavanMeka09 <Mekapavan99@gmail.com>
2026-01-29 16:06:07 +0530

    fix: userData

--- src/lib/userData.ts
+++ src/lib/userData.ts
@@ -2,9 +2,14 @@ import { auth } from '@/lib/auth'
 
 export const getCurrentUser = async () => {
   const session = await auth()
+
+  if (!session?.user?.email) {
+    return null;
+  }
+
   return {
-    name: session?.user.name,
-    email: session?.user.email,
-    image: session?.user.image
+    name: session?.user?.name,
+    email: session?.user?.email,
+    image: session?.user?.image
   }
 }
