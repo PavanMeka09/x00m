563f761abd7d0b41a0e6131e11aecda9269e6cc5
PavanMeka09 <Mekapavan99@gmail.com>
2026-01-29 11:06:11 +0530

    fix: remove create api

--- src/app/api/create/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  const body = await req.json()
-  const id = crypto.randomUUID()
-  return NextResponse.json({
-    id
-  })
-}
